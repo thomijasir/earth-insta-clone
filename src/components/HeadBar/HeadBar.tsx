f4e2d46f91ac4a13f0c8bcba4c04c7efacf4c35f
Thomi Jasir <thomijasir@gmail.com>
2022-05-06 14:47:08 +0700

    Feat(Resource): add design and resource (#1)
    * feat(Resource): add assets and resource
    
    * feat(Headbar): add head bar component
    
    * feat(Test): add test and arrange icon
    
    Co-authored-by: Thomi Jasir <thomi.jasir@dana.id>

---
+++ src/components/HeadBar/HeadBar.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from 'react';
+import INSTA_LOGO_TEXT from '../../assets/icons/instagram-text.jpg';
+import PLUS_ICON from '../../assets/icons/plus-icon.jpg';
+import COMMENT_ICON from '../../assets/icons/comment-icon.jpg';
+import HEART_ICON from '../../assets/icons/heart-icon.jpg';
+import './HeadBar.scss';
+
+export type IHeadBarProps = {};
+
+export const HeadBarDefaultProps = {};
+
+export const HeadBarNamespace = 'HeadBar';
+
+const HeadBar: FC<IHeadBarProps> = () => {
+  return (
+    <div className="head-bar-comp">
+      <div className="col-head-bar insta-logo">
+        <img src={INSTA_LOGO_TEXT} alt="Intagram Logo Text" />
+      </div>
+      <div className="col-head-bar head-icon">
+        <img src={PLUS_ICON} alt="Intagram Logo Text" />
+      </div>
+      <div className="col-head-bar head-icon">
+        <img src={HEART_ICON} alt="Intagram Logo Text" />
+      </div>
+      <div className="col-head-bar head-icon">
+        <img src={COMMENT_ICON} alt="Intagram Logo Text" />
+      </div>
+    </div>
+  );
+};
+
+HeadBar.displayName = HeadBarNamespace;
+HeadBar.defaultProps = HeadBarDefaultProps;
+export default React.memo(HeadBar);
--- src/container/Home/Home.page.tsx
+++ src/container/Home/Home.page.tsx
@@ -3,6 +3,7 @@ import { AppContext } from '../../store/AppProvider';
 import { TITLE_HOME_PAGE } from '../../constants';
 import reactLogo from '../../assets/images/react.svg';
 import './Home.scss';
+import HeadBar from '../../components/HeadBar/HeadBar';
 
 export interface IProps {}
 
@@ -16,20 +17,8 @@ const Home: FC<IProps> = () => {
   }, []);
 
   return (
-    <div className="home-page safe-area">
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-lg-12">
-            <img src={reactLogo} className="rotate" alt="react-logo" />
-            <h1>{title}</h1>
-            <p>
-              Project Template with the latest react hook, simple clean and no
-              vulnerability package
-            </p>
-            <small>React Template by Thomi Jasir</small>
-          </div>
-        </div>
-      </div>
+    <div className="home-page">
+      <HeadBar />
     </div>
   );
 };
