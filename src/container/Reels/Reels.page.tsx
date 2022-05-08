2e94eddd54948da619178f53cf274e0f0a8b25b9
Thomi Jasir <thomijasir@gmail.com>
2022-05-08 15:17:11 +0700

    feat(Reels): add instagram reels (#4)
    Co-authored-by: Thomi Jasir <thomi.jasir@dana.id>

---
+++ src/container/Reels/Reels.page.tsx
@@ -0,0 +1,76 @@
+import React, { FC, useState, useTransition } from 'react';
+import ReactPlayer from 'react-player';
+import './Reels.scss';
+
+export interface IProps {}
+
+const Reels: FC<IProps> = () => {
+  const reelVideoHeight = window.innerHeight - 67;
+  const [reelsPending, reelsTransition] = useTransition();
+  const [reels, setReels] = useState([
+    {
+      id: 1,
+      src: '/resources/reels/tallest-man.mp4',
+      play: true,
+    },
+    {
+      id: 2,
+      src: '/resources/reels/clean-meat.mp4',
+      play: false,
+    },
+    {
+      id: 3,
+      src: '/resources/reels/biggest-tides.mp4',
+      play: false,
+    },
+    {
+      id: 4,
+      src: '/resources/reels/thomi-dancing.mp4',
+      play: false,
+    },
+  ]);
+
+  let timerScroll: any;
+
+  const handleSnapScroll = (e: any) => {
+    const elem = e.target;
+    const getIndex = Math.round(elem.scrollTop / reelVideoHeight);
+    clearTimeout(timerScroll);
+    timerScroll = setTimeout(() => {
+      onSnap(getIndex);
+    }, 400);
+  };
+
+  const onSnap = (index: number) => {
+    reelsTransition(() => {
+      setReels((prevState: any) => {
+        return prevState.map((item: any, rIndex: number) => ({
+          ...item,
+          play: index === rIndex,
+        }));
+      });
+    });
+  };
+
+  return (
+    <div className="col-home home-group reels-page" onScroll={handleSnapScroll}>
+      {reels.map((item: any) => (
+        <div
+          key={item.id}
+          className="reels-item"
+          style={{ height: reelVideoHeight }}
+        >
+          <ReactPlayer
+            width="100%"
+            height="100%"
+            url={item.src}
+            playing={item.play}
+            loop
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Reels;
--- src/services/Router.tsx
+++ src/services/Router.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from '../container/Home/Home.page';
 import ExplorerPage from '../container/Explorer/Explorer.page';
+import ReelsPage from '../container/Reels/Reels.page';
 
 const Router: FC<{}> = () => {
   return (
@@ -9,8 +10,11 @@ const Router: FC<{}> = () => {
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route path="explore" element={<ExplorerPage />} />
-          <Route path="reels" element={<p>Reels Page</p>} />
-          <Route path="profile" element={<p>Profile Page</p>} />
+          <Route path="reels" element={<ReelsPage />} />
+          <Route
+            path="profile"
+            element={<p style={{ margin: 'auto' }}>Profile Page</p>}
+          />
         </Route>
         <Route path="/others" element={<p>Others</p>} />
         <Route path="*" element={<p>404 Page</p>} />
